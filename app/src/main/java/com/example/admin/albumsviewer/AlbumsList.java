ba65ea97a634957574addbb81dc8a25e4d8b24e0
Rafał Zawadzki <rafal.zawadzki@savangard.com>
2018-06-05 22:57:26 +0200

    Added logout mechanism

--- app/src/main/java/com/example/admin/albumsviewer/AlbumsList.java
+++ app/src/main/java/com/example/admin/albumsviewer/AlbumsList.java
@@ -1,13 +1,49 @@
 package com.example.admin.albumsviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AlbumsList extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.albums_list);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setTitle("Biblioteka albumów");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int resID = item.getItemId();
+        if (resID == R.id.logout){
+            // mechanizm wylogowania z firebase
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(AlbumsList.this, "Wylogowałeś się!", Toast.LENGTH_SHORT).show();
+
+            startActivity(new Intent(AlbumsList.this, MainActivity.class));
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/admin/albumsviewer/MainActivity.java
+++ app/src/main/java/com/example/admin/albumsviewer/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -81,6 +84,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int resID = item.getItemId();
+        if (resID == R.id.logout){
+            // mechanizm wylogowania z firebase
+            Toast.makeText(MainActivity.this, "Zaimplementowac wylogowanie", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
 }
